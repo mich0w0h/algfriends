a6ec35974d4ed4c9df3dae46abd346ee5e7a7bcf
mich0w0h <aokimiho71@gmail.com>
2022-01-31 12:29:56 +0900

    set a goal point for moveforward action

--- game/src/components/Scene1.vue
+++ game/src/components/Scene1.vue
@@ -13,23 +13,41 @@ const app: PIXI.Application = new PIXI.Application({
 document.body.appendChild(app.view);
 
 // props
-const goalY = app.screen.height * 0.2;
-const goalScale = 0.2;
+const stepToGoal: number = 15;
+const goalY: number = app.screen.height * 0.25;
+const goalScale: number = 0.2;
+let stepNum: number = 0;
+
+const playerStartPos: PIXI.ObservablePoint = new PIXI.ObservablePoint(
+  () => {},
+  null,
+  app.screen.width * 0.5,
+  app.screen.height * 0.7
+);
 
 // create player object
 const player = PIXI.Sprite.from("/src/assets/images/animal_dog_back.png");
 player.anchor.set(0.5);
-player.position.set(app.screen.width * 0.5, app.screen.height * 0.7);
+player.position.set(playerStartPos.x, playerStartPos.y);
 app.stage.addChild(player);
 
 // Make stage interactive so you can click on it too
 app.stage.interactive = true;
 app.stage.hitArea = app.renderer.screen;
 
+// click to moveforward player
 app.stage.addListener("click", (e) => {
-  player.scale.set(player.scale.x * 0.85, player.scale.y * 0.85);
-  player.position.y -= 70;
+  moveForward();
 });
+
+function moveForward() {
+  if (stepNum === stepToGoal) {
+    return;
+  }
+  stepNum += 1;
+  player.scale.set(player.scale.x - (1 - goalScale) / stepToGoal);
+  player.position.y += (goalY - playerStartPos.y) / stepToGoal;
+}
 </script>
 
 <template></template>
